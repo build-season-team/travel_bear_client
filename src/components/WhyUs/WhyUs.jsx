9fa109893efb8a5b06d58e744485cd70cbccfb3b
lulu <susannwagwu4@gmail.com>
2022-04-05 14:05:22 +0100

    why us

---
+++ src/components/WhyUs/WhyUs.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import classes from './WhyUs.module.css';
+import apartment_rent from  './../assets/images/Apartment-rent.png';
+
+const WhyUs = () => {
+  return (
+    <div>
+        <h1>Why book with TravelBear?</h1>
+        <div>
+            <img src={apartment_rent} alt="" />
+            <h4></h4>
+            <p></p>
+        </div>
+        <div>
+            <img src="" alt="" />
+            <h4></h4>
+            <p></p>
+        </div>
+        <div>
+            <img src="" alt="" />
+            <h4></h4>
+            <p></p>
+        </div>
+    </div>
+  )
+}
+
+export default WhyUs
