3ed8e032db5f65e6db81491a018196085250614b
code_max <ogbujigodwinmax@gmail.com>
2022-04-05 14:01:59 +0100

    feature: created reusable button component

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import './App.css';
+import Button from './components/UI/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Router >
+        <Routes>
+          <Route path='/components' element={<div>
+            <Button name='primary' primary link='/about'  />
+            <br></br> <br></br>
+            <Button name='secondary' secondary/>
+             <br></br> <br></br>
+            <Button name='danger' danger  />
+             <br></br> <br></br>
+            <Button name='dark' dark />
+             <br></br> <br></br>
+            
+            </div>}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UI/FormInput/Index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FormInput = ({inputType, }) => {
+  return (
+    <div>FormInput</div>
+  )
+}
+
+export default FormInput
