6be381ea1b55f72c348e969048110dbdb13a48ad
Tochukwu Jonah <jonahtochukwu@gmail.com>
2022-04-06 15:24:24 +0100

    Revert "input component"
    This reverts commit bc87b24d333073922ed719c5a342e598738222ea.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Button from './components/UI/Button';
-import FormInput from './components/UI/FormInput/Index';
-
-import CheckMark from './assets/icons/check-mark.svg'
 
 function App() {
   return (
@@ -21,14 +18,6 @@ function App() {
              <br></br> <br></br>
             
             </div>}></Route>
-            <Route path='/input' element={ <div> 
-              <FormInput type='password' placeholder={'enter password'}  children />
-              <br />
-              <FormInput label={'Password'} type='text' disabled={true} placeholder={'enter password'}  />
-              <br />
-              <FormInput type='text' required={true} placeholder={'enter number'} text={'Ensure you enter correct details!!!'} />
-              <FormInput type='date' placeholder={'enter password'}  />
-            </div>  } > </Route>
         </Routes>
       </Router>
     </div>
--- src/components/UI/FormInput/Index.jsx
+++ src/components/UI/FormInput/Index.jsx
@@ -1,30 +1,8 @@
 import React from 'react'
-import classes from './FormInput.module.css'
 
-import CheckMark from '../../../assets/icons/check-mark.svg'
-
-const FormInput = ({id, name, label, text, active, required, disabled, value, type, size = 'medium', className = '', icon, onChange, iconPosition, placeholder,
-  ...props }) => {
+const FormInput = ({inputType, }) => {
   return (
-    <div className={classes.form_input}>
-      
-        {label && <label className={classes.label} htmlFor={id}>{label}</label>}
-        <input 
-          type={type} 
-          id={id} 
-          placeholder={placeholder}
-          value={value}
-          name={name}
-          disabled={disabled ? true : false}
-          required={required ? true : false}
-          className={className}
-          onChange={onChange}
-          label={label}
-          icon={icon}
-          
-        />
-        <p className={classes.some_copy}>{text}</p>
-    </div>
+    <div>FormInput</div>
   )
 }
 
