dab0fab79d34e44eea0b76724575ac1896d7fe48
lulu <susannwagwu4@gmail.com>
2022-04-06 14:58:25 +0100

    created why us component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Button from './components/UI/Button';
+import WhyUs from './components/WhyUs/WhyUs.jsx'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
              <br></br> <br></br>
             
             </div>}></Route>
+            
         </Routes>
       </Router>
     </div>
