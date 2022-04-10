9ae25d306c5a62514fd944a22e4672da14f87acf
code_max <ogbujigodwinmax@gmail.com>
2022-04-11 00:32:33 +0100

    feature: made shortlet card and buttons fully responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,6 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
-import Button from './components/UI/Button';
 
-import WhyUs from './components/WhyUs/WhyUs.jsx'
-
-import FormInput from './components/UI/FormInput/Index';
-import Select from './components/UI/Select/select'
-import Update from './components/Update/update'
-import Header from './components/UI/Header/index'
-
-
-import DollarIcon from './assets/icons/dollar-circle.svg';
-import Toast from './components/UI/Toast';
-import Divider from './components/UI/Divider';
-import ShortletCard from './components/UI/ShortletCard';
 
 
 function App() {
