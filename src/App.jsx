28fc2e8517cd53204aa1b19db9b5e90413203d92
lulu~~ <susannwagwu4@gmail.com>
2022-04-13 16:34:10 +0100

    done with landinp page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Header from './components/UI/Header/index';
 import Update from './components/Update/update';
+import LandingPage from './pages/landingPage/Index';
 
 
 
@@ -11,7 +12,7 @@ function App() {
       
       <Router >
         <Routes>
-          <Route path='/update' element={<Header />} />
+          <Route path='/landing' element={< LandingPage />} />
           <Route path='/' element={<div>hello world</div>} />
         </Routes>
       </Router>
