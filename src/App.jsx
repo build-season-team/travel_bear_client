ef4b67a1ad057773d2720ace0990ee8936e7b2bd
code_max <ogbujigodwinmax@gmail.com>
2022-04-14 19:35:06 +0100

    resolved conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
 import { useContext } from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
-import Header from './components/UI/Header/index';
-import Footer from './components/UI/Footer';
-import HeroSection from './components/UI/HeroSection/HeroSection';
-import SearchBar from './components/UI/SearchBar/SearchBar';
-import Update from './components/Update/update';
-import Nav from './components/UI/Header/nav';
 import SignUp from './pages/authentication/sign-up';
 import Login from './pages/authentication/login';
-import ShortletCard from './components/UI/ShortletCard';
-import ErrorPage from './components/UI/404page'
 import LandingPage from './pages/landingPage';
 import ErrorPage from './components/UI/404page'
 import { AuthContext } from './store/authContext/AuthProvider';
