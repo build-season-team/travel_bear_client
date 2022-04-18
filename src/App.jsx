2edd00edd46ae0eb070466a70a4750481bfed3b2
lulu~~ <susannwagwu4@gmail.com>
2022-04-18 13:05:50 +0100

    responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './App.css';
 import Header from './components/UI/Header/index';
-import Footer from './components/UI/Footer';
 import HeroSection from './components/UI/HeroSection/HeroSection';
 import SearchBar from './components/UI/SearchBar/SearchBar';
 import Update from './components/Update/update';
-import Nav from './components/UI/Header/nav';
 import SignUp from './pages/authentication/sign-up';
+import Terms from './pages/T&C/Terms'
 
 
 
@@ -26,6 +25,7 @@ function App() {
           </div> } />
           <Route path='/' element={<div>hello world</div>} />
           <Route path='/signup' element={<SignUp />} />
+          <Route path='/terms' element={<Terms/>}></Route>
         </Routes>
       </Router>
     </div>
