f6f5c9ecdb3355a1e7ce70d485737196d0e5fc1c
code_max <ogbujigodwinmax@gmail.com>
2022-04-27 09:10:54 +0100

    fixed bug

--- src/pages/UploadShortlet/UploadShortlet.jsx
+++ src/pages/UploadShortlet/UploadShortlet.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
 import { FaAngleLeft } from "react-icons/fa";
 
 import FormInput from '../../components/UI/FormInput/Index'
